6e63d9a27d2132e04999ba108217dc78f59748b1
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2024-09-16 23:35:39 +0300

    ChatListItemElement: loading indicator

--- src/ChatListItemElement.ts
+++ src/ChatListItemElement.ts
@@ -8,13 +8,14 @@ import { createElement } from "./createElement";
 export class ChatListItemElement extends Element {
   active_chat_store = ActiveChatStore.get();
 
-  name = createElement("div");
-  model = createElement("i");
+  name = createElement("div", { className: "name" });
+  model = createElement("i", { className: "model" });
+  loader = createElement("div", { className: "loader" });
 
   constructor(readonly chat: Chat) {
     super();
     this.tabIndex = 0;
-    this.append(this.name, this.model);
+    this.append(this.name, this.model, this.loader);
     this.bind(this.chat, "change");
     this.bind(this.active_chat_store, "change");
     this.onclick = () => this.active_chat_store.set(this.chat.id);
@@ -26,6 +27,8 @@ export class ChatListItemElement extends Element {
     const selected = this.active_chat_store.chat_id === this.chat.id;
     if (selected) this.classList.add("selected");
     else this.classList.remove("selected");
+    if (this.chat.last_message?.loading) this.loader.classList.add("loading");
+    else this.loader.classList.remove("loading");
     this.title = this.name.innerText = this.chat.title;
     this.model.innerText = this.chat.model;
   }
