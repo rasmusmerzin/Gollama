4d15c2956bd8252d54f80ee8ebbf1fa96c952d33
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2024-09-26 10:10:27 +0300

    ImageModalElement

--- src/ImageElement.ts
+++ src/ImageElement.ts
@@ -1,6 +1,7 @@
 import "./ImageElement.css";
 import { Element } from "./Element";
 import { createElement } from "./createElement";
+import { ImageModalElement } from "./ImageModalElement";
 
 export interface ImageOptions {
   src: string;
@@ -9,78 +10,19 @@ export interface ImageOptions {
 }
 
 export class ImageElement extends Element {
-  opened = false;
-
   img = createElement("img");
-  left = createElement("div", { className: "left" });
-  right = createElement("div", { className: "right" });
 
   constructor({ src, alt, listed }: Partial<ImageOptions> = {}) {
     super();
     if (src) this.img.src = src;
     if (listed) this.setAttribute("listed", "");
     this.img.alt = alt || "Image";
-    this.append(this.img, this.left, this.right);
-    this.left.onclick = this.leftClick.bind(this);
-    this.right.onclick = this.rightClick.bind(this);
-    this.onblur = this.close.bind(this);
-    this.onclick = this.toggle.bind(this);
-    this.onkeydown = this.keydown.bind(this);
-  }
-
-  toggle() {
-    this.opened = !this.opened;
-    this.render();
-  }
-
-  close() {
-    this.opened = false;
-    this.render();
-  }
-
-  leftClick(event: Event) {
-    event.stopPropagation();
-    this.getNeighbors().previous?.click();
-  }
-
-  rightClick(event: Event) {
-    event.stopPropagation();
-    this.getNeighbors().next?.click();
-  }
-
-  keydown(event: Event) {
-    const { key, shiftKey } = event as KeyboardEvent;
-    const arrow_previous = ["ArrowLeft", "ArrowUp"].includes(key);
-    const arrow_next = ["ArrowRight", "ArrowDown"].includes(key);
-    if (arrow_previous || (key === "Tab" && shiftKey)) {
-      event.preventDefault();
-      this.getNeighbors().previous?.click();
-    } else if (arrow_next || (key == "Tab" && !shiftKey)) {
-      event.preventDefault();
-      this.getNeighbors().next?.click();
-    }
-  }
-
-  render() {
-    if (this.opened) {
-      this.img.onclick = (e) => e.stopPropagation();
-      const { previous, next } = this.getNeighbors();
-      if (previous) this.left.classList.remove("disabled");
-      else this.left.classList.add("disabled");
-      if (next) this.right.classList.remove("disabled");
-      else this.right.classList.add("disabled");
-      this.classList.add("open");
-      this.tabIndex = 0;
-      this.focus();
-    } else {
-      this.img.onclick = null;
-      this.tabIndex = -1;
-      this.classList.remove("open");
-    }
+    this.append(this.img);
+    this.onclick = () => new ImageModalElement(this);
   }
 
   getNeighbors() {
-    let siblings = new Array<HTMLElement>();
+    let siblings = new Array<ImageElement>();
     if (this.hasAttribute("listed")) {
       const query = "image-element[listed]";
       siblings = Array.from(document.querySelectorAll(query));
@@ -90,8 +32,8 @@ export class ImageElement extends Element {
       );
     }
     const index = siblings.findIndex((e) => e === this);
-    let previous: HTMLElement | null = null;
-    let next: HTMLElement | null = null;
+    let previous: ImageElement | null = null;
+    let next: ImageElement | null = null;
     if (index > 0) previous = siblings[index - 1];
     if (index < siblings.length - 1) next = siblings[index + 1];
     return { previous, next };
---
+++ src/ImageModalElement.ts
@@ -0,0 +1,48 @@
+import "./ImageModalElement.css";
+import { ImageElement } from "./ImageElement";
+import { Modal } from "./Modal";
+import { createElement } from "./createElement";
+
+export class ImageModalElement extends Modal {
+  img = createElement("img");
+  left = createElement("button", {
+    className: "material-icons left",
+    innerText: "chevron_left",
+  });
+  right = createElement("button", {
+    className: "material-icons right",
+    innerText: "chevron_right",
+  });
+
+  constructor(private element: ImageElement) {
+    super();
+    this.replaceChildren(this.img, this.left, this.right);
+    this.img.onclick = (e) => e.stopPropagation();
+    this.left.onclick = this.leftClick.bind(this);
+    this.right.onclick = this.rightClick.bind(this);
+    this.render();
+  }
+
+  leftClick(event: Event) {
+    event.stopPropagation();
+    const { previous } = this.element.getNeighbors();
+    if (previous) this.element = previous;
+    this.render();
+  }
+
+  rightClick(event: Event) {
+    event.stopPropagation();
+    const { next } = this.element.getNeighbors();
+    if (next) this.element = next;
+    this.render();
+  }
+
+  render() {
+    const { next, previous } = this.element.getNeighbors();
+    this.img.src = this.element.img.src;
+    this.left.disabled = !previous;
+    this.right.disabled = !next;
+  }
+}
+
+customElements.define("image-modal-element", ImageModalElement);
